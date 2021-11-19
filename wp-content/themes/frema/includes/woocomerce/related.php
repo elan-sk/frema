96912c57d9b8473424e5727ffc97d4df9ed76422
elan-sk <elan-sk@hotmail.com>
2021-11-19 07:50:38 -0500

    Carrusel de productos insertado

--- wp-content/themes/frema/includes/woocomerce/related.php
+++ wp-content/themes/frema/includes/woocomerce/related.php
@@ -5,22 +5,11 @@
 			<header class="site-main__header">
 				<h2>Productos destacados</h2>
 			</header>
-
-			<?php
-			/**
-			 * Functions hooked in to homepage action
-			 *
-			 * @hooked storefront_homepage_content      - 10
-			 * @hooked storefront_product_categories    - 20
-			 * @hooked storefront_recent_products       - 30
-			 * @hooked storefront_featured_products     - 40
-			 * @hooked storefront_popular_products      - 50
-			 * @hooked storefront_on_sale_products      - 60
-			 * @hooked storefront_best_selling_products - 70
-			 */
-				echo do_shortcode( '[recent_products per_page="8" columns="3" category="destacado"]' );
-			?>
-
+			<div style="transform: skewY(3deg)">
+				<?php
+					echo do_shortcode( '[woorousell id="510"]' );
+				?>
+			</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
